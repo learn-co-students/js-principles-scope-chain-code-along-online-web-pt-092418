cb0cfd437e3a0f7c33ef7bcfcc2fc4242a3e12cc
Carolyn Whelpley <carolynwhelpley@hotmail.com>
2019-04-22 09:41:16 -0400

    Interim checkin

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 /* Your code goes here */
+var customerName = 'bob'
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase()
+  return customerName 
+};
+
+function setBestCustomer() {
+  bestCustomer = 'not bob'
+};
+function overWriteBestCustomer() {
+  bestCustomer = 'maybe bob'
+};
+function changeLeastFavoriteCustomer() {
+  LeastFavoriteCustomer = bestCustomer()
+};
+
+function attemptTwoFavoriteCustomers() {
+  let favoriteCustomer = '';
+
+  return favoriteCustomer
+
+  let favoriteCustomer = '';
+  
+  return favoriteCustomer
+};
